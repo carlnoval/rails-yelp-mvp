adcb507c6c5b733af5eafe685604b8d9fa331a8b
Carl Noval <cjrnoval22@gmail.com>
2022-01-11 21:22:25 +0900

    rake passed for both models: Restaurant and Review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,20 @@
 class Restaurant < ApplicationRecord
+  # A restaurant must have a name, an address and a category.
+  # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+  # When a restaurant is destroyed, all of its reviews must be destroyed as well.
+
+  # rails g model Restaurant name address phone_number category
+
+  # deleting a restaurant will also delete all reviews related to it
+  has_many :reviews, dependent: :destroy
+
+  # presence validation
+  validates :name, :address, :phone_number, presence: :true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: "must be one of: Chinese, Italian, Japanese, French, Belgian" }
+  validates :phone_number, format: { with: /\A^[-\(\)\d\.\ ]+\z/, message: "only allows numbers and these characters: ()-. and space" }
+
+  def em
+    self.save
+    self.errors.messages
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,19 @@
 class Review < ApplicationRecord
+  # A review must belong to a restaurant.
+  # A review must have content and a rating.
+  # A review’s rating must be a number between 0 and 5.
+
+  # rails g model Review rating:integer content:text restaurant:references
+
   belongs_to :restaurant
+
+  # inclusion is to make sure that the rating is saved as within 0 to 5 but does not take strings into account...
+  # that is where numericality comes to play
+  validates :rating, presence: true, inclusion: { in: (0..5), message: "rating must be from 0 to 5 only" }, numericality: { only_integer: true }
+  validates :content, presence: true,  length: { minimum: 20, message: "must at least be 20 characters" }
+
+  def em
+    self.save
+    self.errors.messages
+  end
 end
